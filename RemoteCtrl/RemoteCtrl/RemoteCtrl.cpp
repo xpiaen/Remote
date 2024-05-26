247a4fd539b24e302ff899f1764031fe509ac285
xp <1510139327@qq.com>
2024-05-26 16:42:45 +0800

    初步的网络编程框架搭建完成

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -4,6 +4,7 @@
 #include "pch.h"
 #include "framework.h"
 #include "RemoteCtrl.h"
+#include "ServerSocket.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -32,8 +33,28 @@ int main()
             nRetCode = 1;
         }
         else
-        {
-            // TODO: 在此处为应用程序的行为编写代码。
+        {//1 进度可控性 2 方便对接  3 可行性评估，提早暴露风险
+            // TODO: socket bind listen accept read write close
+            //套接字初始化
+            //server;
+            CServerSocket* pserver = CServerSocket::getInstance();
+            if (pserver->InitSocket() == false) {
+                MessageBox(NULL, _T("网络初始化异常，未能成功初始化网络，请检查网络状态"), _T("网络初始化失败！"), MB_OK | MB_ICONERROR);
+                exit(0);
+            }
+            int count = 0;
+            while (CServerSocket::getInstance() != NULL) {
+                
+                if (pserver->AcceptClient() == false) {
+                    if (count >= 3) {
+                        MessageBox(NULL, _T("多次无法正常接入用户，结束程序"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
+                        exit(0);
+                    }
+                    MessageBox(NULL, _T("无法正常接入用户，自动重试"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
+                    count++;
+                }
+                int ret = pserver->DealCommand();
+            }
         }
     }
     else
