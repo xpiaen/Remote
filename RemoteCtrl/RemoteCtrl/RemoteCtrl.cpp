471635d0cf794872a359eee96f80dfd7ae9a5ce3
xp <1510139327@qq.com>
2024-06-28 17:03:15 +0800

    1 线程安全的队列模板的实现

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -7,7 +7,7 @@
 #include "ServerSocket.h"
 #include "Command.h"
 #include <conio.h>
-
+#include "CEdoyunQueue.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -88,15 +88,12 @@ void threadmain(HANDLE hIOCP)
         }
         else if (pParam->nOperator == IocpListPop) {
             printf("pop from list:%p size %d\r\n", pParam->cbFunc, listString.size());
-            //std::string* pStr = NULL;
             std::string str;
             if (listString.size() > 0) {
-                //pStr = new std::string(listString.front());
                 str = listString.front();
                 listString.pop_front();
             }
             if (pParam->cbFunc) {
-                //pParam->cbFunc(pStr);
                 pParam->cbFunc(&str);
             }
             count0++;
@@ -131,38 +128,24 @@ int main()
     if (!CEdoyunTools::Init())return 1;
     
     printf("press any key to exit...\r\n");
-    HANDLE hIOCP = INVALID_HANDLE_VALUE;//IO Completion Port
-    hIOCP = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, NULL, 1);//创建IOCP    epoll的区别点1
-    if (hIOCP == INVALID_HANDLE_VALUE || hIOCP == NULL) {
-        printf("CreateIoCompletionPort failed with %d\n", GetLastError());
-        return 1;
-    }
-    HANDLE hThread = (HANDLE)_beginthread(threadQueueEntry, 0, hIOCP);//启动IOCP线程
-
-    ULONGLONG tick = GetTickCount64();
-    ULONGLONG tick0 = GetTickCount64();
-    int count = 0, count0 = 0;
+    CEdoyunQueue<std::string> lstStrings;
+    ULONGLONG tick = GetTickCount64(),tick0 = GetTickCount64();
     while (_kbhit() == 0) {//完成端口 把请求与实现 分离了
         if (GetTickCount64() - tick0 > 1300) {
-            PostQueuedCompletionStatus(hIOCP, sizeof(IOCP_PARAM), (ULONG_PTR)new IOCP_PARAM(IocpListPop, "hello world!",func), NULL);
+            lstStrings.PushBack("hello world!");
             tick0 = GetTickCount64();
-            count0++;
         }
         if (GetTickCount64() - tick > 2000) {
-            PostQueuedCompletionStatus(hIOCP, sizeof(IOCP_PARAM), (ULONG_PTR)new IOCP_PARAM(IocpListPush, "hello world!"), NULL);//通知IOCP线程退出
+            std::string str;
+            lstStrings.PopFront(str);
             tick = GetTickCount64();
-            count++;
+            printf("pop from list:%s\r\n", str.c_str());
         }
         Sleep(1);
     }
-    if (hIOCP != NULL) {
-        PostQueuedCompletionStatus(hIOCP, 0, NULL, NULL);//通知IOCP线程退出
-        WaitForSingleObject(hThread, INFINITE);//等待IOCP线程退出
-    }
-
-    CloseHandle(hIOCP);
-
-    printf("exit done!count: %d, count0: %d\r\n", count, count0);
+    printf("exit done!size: %d\r\n", lstStrings.Size());
+    lstStrings.Clear();
+    printf("exit done!size: %d\r\n", lstStrings.Size());
     ::exit(0);
     /*
     if (CEdoyunTools::IsAdmin()) {
